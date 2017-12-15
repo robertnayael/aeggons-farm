c1a3e6c0b801045505730819683d3dece2f5f2af
Robert Gulewicz <nayael@wp.pl>
2017-12-15 18:11:55 +0100

    Refactored overlay effects.

--- app/renderers/effects.js
+++ app/renderers/effects.js
@@ -19,16 +19,97 @@ export function fadeIn(duration) {
   };
 }
 
-export function circleOut(duration, targetRadius) {
 
-  let step = targetRadius/duration;
-  const accelleration = 1.02;
-  let radius = 0;
+
+/*----------------------------------------------------------------------------*/
+/**
+ *
+ */
+export function circleOut(ctx, width, height, overlay, steps) {
+console.log('im being called: circleOut');
+
+  const cornerToCenter = Math.sqrt( Math.pow((width), 2) + Math.pow((height), 2) ) / 2; // Max. radius
+  const startingRadius = 0,
+        targetRadius = cornerToCenter;
+
+  return circleInOut.apply(null, [...arguments, 0, cornerToCenter]);
+}
+
+/*----------------------------------------------------------------------------*/
+/**
+ *
+ */
+function circleInOut(ctx, width, height, overlay, steps, startingRadius, targetRadius) {
+
+  const getRadius = stepFromTo(startingRadius, targetRadius, steps);
+
+  return function() {
+    const radius = getRadius();
+    const animationIsFinished = radius === targetRadius;
+
+      drawHole(ctx, width, height, radius, overlay);
+
+    if(animationIsFinished) return true;
+  };
+}
+
+/*----------------------------------------------------------------------------*/
+/**
+ *
+ */
+function drawHole(ctx, width, height, radius, overlay) {
+    ctx.save();
+
+    // Plot the circle path inside:
+    ctx.beginPath();
+    ctx.arc(width/2, height/2, radius, 0, Math.PI*2, false);
+    ctx.closePath();
+
+    // Plot the square path outside:
+    ctx.moveTo(0,0);
+    ctx.lineTo(0, height);
+    ctx.lineTo(width,height);
+    ctx.lineTo(width,0);
+    ctx.closePath();
+
+    ctx.clip();
+
+    ctx.fillStyle = overlay;
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.restore();
+}
+
+/*----------------------------------------------------------------------------*/
+/**
+ *
+ */
+function drawOverlay(ctx, overlay) {
+
+
+}
+
+/*----------------------------------------------------------------------------*/
+/**
+ * @param  {number} start             initial value
+ * @param  {number} end               end value
+ * @param  {number} steps             number of steps to achieve the end value
+ * @param  {number} accelleration = 1 acceleration factor for the increase step
+ * @return {number}                   current value
+ */
+function stepFromTo(start, end, steps, accelleration = 1) {
+  let current = start;
+  let step = ((end - start) / steps);
 
   return function() {
     step = step * accelleration;
-    radius += step;
-    if (radius > targetRadius) radius = targetRadius;
-    return radius;
+    current += step;
+
+    if (start < end && current > end
+    ||  start > end && current < end) {
+      current = end;
+    }
+
+    return current;
   };
 }
--- app/renderers/gameplayIntro.js
+++ app/renderers/gameplayIntro.js
@@ -1,12 +1,20 @@
 import {fadeIn} from './effects';
-import {circleIn} from './effects';
+import {circleOut} from './effects';
 
 let opacity = fadeIn(180);
 let radius;
+let effects = [];
 
 export default function(ctx, justEnabled, scale, controls, game, map, player) {
 
-  if (justEnabled) opacity = fadeIn(180);
+  const width = map.viewport.width.px,
+        height = map.viewport.height.px;
+
+
+  if (justEnabled) {
+    opacity = fadeIn(180);
+    console.log('START!');
+  }
 
 
   ctx.globalAlpha = opacity();
@@ -14,33 +22,11 @@ export default function(ctx, justEnabled, scale, controls, game, map, player) {
   ctx.fillRect(0, 0, map.viewport.width.px, map.viewport.height.px);
   ctx.globalAlpha = 1;
 
+  if (justEnabled) effects[0] = circleOut(ctx, width, height, '#000000', 100);
 
+  if (effects[0]() === true) {
+      //console.log('czas na kolejny efekt');
+  }
 
-var width = map.viewport.width.px;
-var height = map.viewport.height.px;
-
-const cornerToCenter = Math.sqrt( Math.pow((width), 2) + Math.pow((height), 2) ) / 2;
-
-if (justEnabled) radius = circleIn(180, cornerToCenter);
-
-  ctx.save();
-
-  ctx.beginPath();
-  ctx.arc(width/2, height/2, radius(), 0, Math.PI*2, false);
-  ctx.closePath();
-
-  ctx.moveTo(0,0);
-  ctx.lineTo(0, height);
-  ctx.lineTo(width,height);
-  ctx.lineTo(width,0);
-
-  ctx.closePath();
-
-  ctx.clip();
-
-  ctx.fillStyle = '#000000';
-  ctx.fillRect(0, 0, map.viewport.width.px, map.viewport.height.px);
-
-  ctx.restore();
 
 }
