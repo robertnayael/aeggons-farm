7c1c50fb8a82a798f4f4275feec07383c6792f8b
Robert Gulewicz <nayael@wp.pl>
2018-02-20 22:41:56 +0100

    Convert entities: mobs and platforms

--- map_converter/convert.js
+++ map_converter/convert.js
@@ -1,6 +1,7 @@
 const MAP_FILE = './map.tmx',
-      OUTPUT_FILE_TILES = '../dist/data/tiles.json',
-      OUTPUT_FILE_ENTITIES = '../dist/data/__entities.json';
+      OUTPUT_FILE_TILES = '../dist/data/map.json',
+      OUTPUT_FILE_ENTITIES = '../dist/data/entities.json',
+      BASE_TILE_SIZE = 70;
 
 const tmx = require('tmx-parser'),
       fs = require('fs');
@@ -19,11 +20,14 @@ tmx.parseFile(MAP_FILE, function(err, map) {
         entities: convertEntityLayers(entityLayers, map.tileWidth)
     };
 
-    console.log(output.entities);
-
     fs.writeFile(OUTPUT_FILE_TILES, JSON.stringify(output.tiles), err => {
         if (err) throw err;
         console.log(`Map tiles saved as <${OUTPUT_FILE_TILES}>`);
+    });
+
+    fs.writeFile(OUTPUT_FILE_ENTITIES, JSON.stringify(output.entities), err => {
+        if (err) throw err;
+        console.log(`Map tiles saved as <${OUTPUT_FILE_ENTITIES}>`);
     }); 
 
 });
@@ -46,7 +50,7 @@ function convertTileLayers(layers) {
     }, {});
 }
 
-function convertEntityLayers(layers, tileSize) {
+function convertEntityLayers(layers) {
     return layers.reduce((layers, layer) => {
         const {type} = layer.properties,
               entities = layer.objects;
@@ -55,6 +59,8 @@ function convertEntityLayers(layers, tileSize) {
         switch (type) {
             case 'platforms': converted = convertPlatformEntities(entities); break;
             case 'mobs': converted = convertMobEntities(entities); break;
+            case 'spikes': break;
+            case 'infoSigns': break;
         }
 
         layers[type] = converted;
@@ -63,10 +69,105 @@ function convertEntityLayers(layers, tileSize) {
 }
 
 function convertMobEntities(entities) {
-    return 'mobs!';
+    return entities.reduce((mobs, mob) => {
+        const {type} = mob.properties;
+        mob.properties.width = mob.properties.width || 1;   // default size: 1 tile
+        mob.properties.height = mob.properties.height || 1;
+        return [...mobs,
+            Object.assign(
+                convertLinearlyMovingEntity(mob),
+                {
+                    type: type ? type : 'default'
+                }
+            )
+        ];
+    }, []);
 }
 
 function convertPlatformEntities(entities) {
-    return 'platforms!';
+    return entities.reduce((platforms, platform) => {
+        const {type} = platform.properties;
+        return [...platforms,
+            Object.assign(
+                convertLinearlyMovingEntity(platform),
+                {
+                    type: type ? type : 'default'
+                }
+            )
+        ];
+    }, []);
 }
 
+function convertLinearlyMovingEntity(entity) {
+    const movementBox = convertPxToTiles({
+        x: entity.x,
+        y: entity.y,
+        width: entity.width,
+        height: entity.height
+    }),
+    entityBox = {
+        width: entity.properties.width,
+        height: entity.properties.height
+    },
+    {axis, start, speed} = entity.properties,
+    position = {};
+
+    if (axis === 'vertical') {
+        if (start === 'bottom') {
+            position.initial = {
+                x: movementBox.x,
+                y: movementBox.y + movementBox.height - entityBox.height
+            };
+            position.final = {
+                y: movementBox.y
+            };
+        }
+        // assume starting position: top
+        else {
+            position.initial = {
+                x: movementBox.x,
+                y: movementBox.y
+            };
+            position.final = {
+                y: movementBox.y + movementBox.height - entityBox.height
+            };
+        }
+    }
+    // assume motion axis: horizontal
+    else {
+        if (start === 'right') {
+            position.initial = {
+                x: movementBox.x + movementBox.width - entityBox.width,
+                y: movementBox.y
+            };
+            position.final = {
+                x: movementBox.x
+            };
+        }
+        // assume starting position: left
+        else {
+            position.initial = {
+                x: movementBox.x,
+                y: movementBox.y
+            };
+            position.final = {
+                x: movementBox.x + movementBox.width - entityBox.width
+            };
+        }
+    }
+
+    return {
+        initialPosition: position.initial,
+        finalPosition: position.final,
+        width: entityBox.width,
+        height: entityBox.height,
+        speed: speed
+    };
+}
+
+function convertPxToTiles(dimensions) {
+    return Object.keys(dimensions).reduce((all, curr) => {
+        all[curr] = dimensions[curr] / BASE_TILE_SIZE;
+        return all;
+    }, {});
+};
