01eb4cce3a3af7847e7c3f737128e071f9574a38
Robert Gulewicz <nayael@wp.pl>
2018-03-10 23:39:31 +0100

    Update and return sprites

--- app/entities/Collectible.js
+++ app/entities/Collectible.js
@@ -5,14 +5,26 @@ export default class Collectible extends AnimatedEntity {
   constructor(props, tileSize, scale, sprites) {
     super(props, tileSize, scale, sprites);
     this.collected = false;
+    this.spriteVariant = props.variant || 'default';
+    this.firstAnimationFrame = props.frame || 0;
   }
 
   update(step) {
-    return null;
+    /* Normally, sprite frames are only requested for entities within
+     * the viewing range. This could cause animations to go out of sync
+     * because each collectible can start its animation at
+     * a different frame.
+     * Solution: store the current frame on every update, and getSprite()
+     * will return it if requested.
+     */
+    this.frame = this.getSpriteFrame(
+      ['collectibles', this.spriteVariant, 'uncollected'],
+      this.firstAnimationFrame
+    );
   }
 
   getSprite() {
-    return null;
+    return this.frame;
   }
 
   checkInteraction(player) {
