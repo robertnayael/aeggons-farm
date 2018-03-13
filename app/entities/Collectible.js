c159c32595050de4fc53e1a83b5b51c5eae7eb58
Robert Gulewicz <nayael@wp.pl>
2018-03-13 21:02:27 +0100

    Add special sprite effect when collectible is collected

--- app/entities/Collectible.js
+++ app/entities/Collectible.js
@@ -5,11 +5,14 @@ export default class Collectible extends AnimatedEntity {
   constructor(props, tileSize, scale, sprites) {
     super(props, tileSize, scale, sprites);
     this.collected = false;
+    this.collectedAndDisappeared = false;
     this.spriteVariant = props.variant || 'default';
     this.firstAnimationFrame = props.frame || 0;
   }
 
   update(step) {
+    if (this.collectedAndDisappeared) return;
+
     /* Normally, sprite frames are only requested for entities within
      * the viewing range. This could cause animations to go out of sync
      * because each collectible can start its animation at
@@ -17,10 +20,20 @@ export default class Collectible extends AnimatedEntity {
      * Solution: store the current frame on every update, and getSprite()
      * will return it if requested.
      */
-    this.frame = this.getSpriteFrame(
-      ['collectibles', this.spriteVariant, 'uncollected'],
-      this.firstAnimationFrame
-    );
+    if (!this.collected) {
+      this.frame = this.getSpriteFrame(
+        ['collectibles', this.spriteVariant, 'uncollected'],
+        this.firstAnimationFrame
+      );
+    } else {
+      this.frame = this.getSpriteFrame(
+        ['collectibles', this.spriteVariant, 'collected'],
+        0,
+        false
+      );
+    }
+
+    if (!this.frame) this.collectedAndDisappeared = true;
   }
 
   getSprite() {
@@ -28,15 +41,17 @@ export default class Collectible extends AnimatedEntity {
   }
 
   checkInteraction(player) {
-    if (this.collidesWith(player)) {
+    if (!this.collected && this.collidesWith(player)) {
+      this.collect();
       return [
-        'ADD_COLLECTIBLE',
+        'COLLECTIBLE_COLLECTED',
       ];
     }
   }
 
   collect() {
-
+    this.startEffect(['collectibles', 'effects', 'collected']);
+    this.collected = true;
   }
 
 }
