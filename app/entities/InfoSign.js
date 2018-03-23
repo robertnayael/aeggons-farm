5acf874d445fc1276e734e9043200203eecff103
Robert Gulewicz <nayael@wp.pl>
2018-03-23 15:36:46 +0100

    Add infoSigns

--- app/entities/InfoSign.js
+++ app/entities/InfoSign.js
@@ -5,10 +5,39 @@ import Entity from './base/Entity';
 export default class InfoSign extends Entity {
 
   constructor(props, tileSize, scale, sprites) {
-    super(props, tileSize, scale);
+    super(props, tileSize, scale, sprites);
 
-    this.active = false;
-    this.activeSince = null;
+    this.signType = props.type;
+    this.info = props.info;
+
+    this.opacity = 0;
+    this.opacityStep = 1 / props.opacitySteps;
+    this.isActive = false;
+  }
+
+  updateOpacity(opacitySteps) {
+    const change = this.opacityStep * (this.isActive ? 1 : -1);
+    this.opacity = Math.min(1, Math.max(0, this.opacity + change));
+  }
+
+  update(step) {
+    this.updateOpacity();
+  }
+
+  checkInteraction(player) {
+      this.isActive = this.collidesWith(player);
+  }
+
+  getSprite() {
+    const signBoard = this.sprites.getSprite(['infoSigns', 'signs', this.signType]);
+    if (!this.isActive && !this.opacity) return signBoard;
+
+    const infoBubble = this.sprites.getSprite(['infoSigns', 'info', this.info]);
+    infoBubble.opacity = this.opacity;
+    infoBubble.drawOffsetX = -(infoBubble.width - signBoard.width) / 2;
+    infoBubble.drawOffsetY = -infoBubble.height;
+
+    return [signBoard, infoBubble];
   }
 
 }
