f3f4a647bfda6f1389dd864411b5fbd96a3b3790
Robert Gulewicz <nayael@wp.pl>
2018-04-07 21:56:16 +0200

    Add SpecialObject class

---
+++ app/entities/SpecialObject.js
@@ -0,0 +1,11 @@
+import Entity from './base/Entity';
+
+/******************************************************************************/
+
+export default class SpecialObject extends Entity {
+
+    constructor(props) {
+        super(props);
+    }
+
+}
