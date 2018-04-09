ace7d693fd8721a246442df6df1d8e197d39825c
Robert Gulewicz <nayael@wp.pl>
2018-04-09 23:52:33 +0200

    Check victory conditions and return actions to player

--- app/entities/SpecialObject.js
+++ app/entities/SpecialObject.js
@@ -4,8 +4,27 @@ import Entity from './base/Entity';
 
 export default class SpecialObject extends Entity {
 
-    constructor(props) {
-        super(props);
+    constructor(props, tileSize, scale, sprites, victoryConditionsMet) {
+        super(props, tileSize, scale, sprites);
+
+        this.type = props.type;
+        this.victoryConditionsMet = victoryConditionsMet;
+    }
+
+    checkInteraction(player) {
+        if (!this.collidesWith(player)) return;
+
+        if (!this.victoryConditionsMet()) {
+            // remind the player to get all collectibles first
+        }
+
+        return this.playerActions();
+    }
+
+    playerActions() {
+        switch (this.type) {
+            case 'finish': return ['REACH_FINISH'];
+        }
     }
 
 }
